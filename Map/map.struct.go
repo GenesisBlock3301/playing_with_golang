0a491fe7450236cb8e42aef5f22b05c1018c6c9e
genesisblock3301 <nur15-1463@diu.edu.bd>
2022-04-20 12:55:52 +0600

    map

--- Map/map.struct.go
+++ Map/map.struct.go
@@ -9,7 +9,7 @@ type Vertex struct{
 func main(){
 
 	var m = map[string]Vertex{
-		"javapoint":Vertex{40.67889789,90.67568679678},
+		"javapoint":Vertex{47.67889789,90.67568679678},
 		"SST":Vertex{95.0594054,-78.053458493},
 	}
 	fmt.Println(m["javapoint"].Latitude)
